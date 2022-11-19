f2160c790ba9ec95433f5c8bd3dc25dada67b094
Azat Bakyt <azatbakyt8@gmail.com>
2022-11-19 17:16:35 +0600

    desktop part done

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+var nav_link = header.getElementsByClassName("nav__link");
+
+for (var i = 0; i < nav_link.length; i++) {
+    nav_link[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
